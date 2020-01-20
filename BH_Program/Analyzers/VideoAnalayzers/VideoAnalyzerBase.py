e3515a43a8940761d08aaf198a851b09d587a91a
matan benita <mbjahnoon@gmail.com>
2020-01-20 20:39:39 +0000

    Added AnalyzerUtils

--- BH_Program/Analyzers/VideoAnalayzers/VideoAnalyzerBase.py
+++ BH_Program/Analyzers/VideoAnalayzers/VideoAnalyzerBase.py
@@ -1,4 +1,11 @@
+import sys
+sys.path.append("..")
 from axel import Event
+from BH_Program.Analyzers.VideoAnalayzers.AnalyzerUtils import *
 
 class VideoAnalyzerBase:
    video_analyzed_event = Event()
+
+
+
+
